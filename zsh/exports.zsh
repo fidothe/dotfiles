0d2e3a14f17e0d09c7fdde58872642ea1dddb97f
Matt Patterson <matt@reprocessed.org>
2013-10-07 12:38:19 +0200

    Use the up-to-date certs file from Curl

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -5,3 +5,4 @@ export BUNDLER_EDITOR=vim
 export PATH="/usr/local/share/python:/usr/local/share/npm/bin:/usr/local/bin:${PATH}"
 export NODE_PATH="/usr/local/lib/node_modules:${NODE_PATH}"
 export CLICOLOR=1
+export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
