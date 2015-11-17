ff00f52f2f062608508865026be3ee0771350d60
Matt Patterson <matt@reprocessed.org>
2015-11-17 15:25:39 +0000

    only override SSL_CERT_FILE if we should
    another mac-ism causing problems on Linux

--- sh-common/exports.sh
+++ sh-common/exports.sh
@@ -4,7 +4,10 @@ export GIT_EDITOR="vim -f"
 export BUNDLER_EDITOR=vim
 export NODE_PATH="/usr/local/lib/node_modules:${NODE_PATH}"
 export CLICOLOR=1
-export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
+# If we have homebrew installed CA bundle
+if [ -f /usr/local/opt/curl-ca-bundle/share/ca-bundle.crt ]; then
+  export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
+fi
 export GOPATH=${HOME}/Documents/work/go
 export ANDROID_TOOLS_PATH=${HOME}/Library/Android/sdk/platform-tools
 export PATH="${GOPATH}/bin:${ANDROID_TOOLS_PATH}:/usr/local/share/npm/bin:/usr/local/bin:${PATH}"
