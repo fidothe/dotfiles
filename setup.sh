a50a84451de9a35cff6859ee66100d4ff0ee6b9d
Matt Patterson <matt@reprocessed.org>
2015-11-18 14:27:16 +0100

    Install / switch to zsh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,11 @@ git clone git@github.com:fidothe/dotfiles.git
 cd dotfiles
 ./dotfiles-install
 
+# shell setup
+if which apt-get; then
+  sudo apt-get install -y zsh
+  sudo chsh -s /usr/bin/zsh `whoami`
+fi
+
 # vim setup - initial plugin install
 vim -u ${HOME}/.vim/bundles.vim +PluginInstall +qall
